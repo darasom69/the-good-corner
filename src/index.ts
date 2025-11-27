8a0c8c81374faa805b0c85c227b745810ded140e
darasom69 <daha69@hotmail.fr>
2025-11-27 10:04:24 +0100

    create

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+let ads = [
+  {
+    id: 1,
+    title: "Bike to sell",
+    description:
+      "My bike is blue, working fine. I'm selling it because I've got a new one",
+    owner: "bike.seller@gmail.com",
+    price: 100,
+    picture:
+      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+    location: "Paris",
+    createdAt: "2023-09-05T10:13:14.755Z",
+  },
+  {
+    id: 2,
+    title: "Car to sell",
+    description:
+      "My car is blue, working fine. I'm selling it because I've got a new one",
+    owner: "car.seller@gmail.com",
+    price: 10000,
+    picture:
+      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+    location: "Paris",
+    createdAt: "2023-10-05T10:14:15.922Z",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send(ads);
+});
+
+app.post("/ads", (req, res) => {
+    ads.push(req.body);
+    res.send("Request received, check backend terminal");
+});
+
+app.delete("/ads/:id", (req, res) => {
+    const adId = parseInt(req.params.id);
+    const adToDelete = ads.find(ad => ad.id === adId);
+    if (!adToDelete) return res.status(404);
+ads = ads.filter(ad => ad.id !== adToDelete.id);
+    res.send("Ad deleted");
+});
+
+app.patch("/ads/:id", (req, res) => {
+    const adId = parseInt(req.params.id);
+    const adToUpdate = ads.find(ad => ad.id === adId);
+    if (!adToUpdate) return res.status(404);
+    Object.assign(adToUpdate, req.body);
+    res.send("Ad updated");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
